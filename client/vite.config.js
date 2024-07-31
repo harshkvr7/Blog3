c643ba5d750ed4a4a574cb0117768fcc7fb8e4ef
Harsh <darshnar34@gmail.com>
2024-07-31 14:44:28 +0530

    created dist

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port : 5173,
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://blog3-production-40fd.up.railway.app/",
         changeOrigin: true,
         secure: false,
       },
