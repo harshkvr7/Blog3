67a700858996500cbc8cdec6c95ec58a5567381a
Harsh <darshnar34@gmail.com>
2024-08-11 13:24:45 +0530

    fixed proxy error

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,11 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port : 5173,
+    port: 5173,
     proxy: {
       "/api": {
-        //target: "http://localhost:3000/",
-        target: 'http://host.docker.internal:3000/',
+        target: 'http://api:3000/',  // Docker service name
         changeOrigin: true,
         secure: false,
       },
